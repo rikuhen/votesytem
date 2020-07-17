123b9e741e74619d00825e4aabc8af6153297c78
Jorge Veliz <jorgeconsalvacion@gmail.com>
2020-07-17 08:19:50 -0500

    cambia a cada minuto

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,7 +26,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('send-passwords-voters')->everyFiveMinutes();
+        $schedule->command('send-passwords-voters')->everyMinute();
     }
 
     /**
