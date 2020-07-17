d632e6803cdc2f1c1e26cdce0fb94c953bd5b832
Jorge Veliz <jorgeconsalvacion@gmail.com>
2020-07-17 07:53:47 -0500

    cambia cron a cada 5 minutos

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,7 +26,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('send-passwords-voters')->everyTenMinutes();
+        $schedule->command('send-passwords-voters')->everyFiveMinutes();
     }
 
     /**
